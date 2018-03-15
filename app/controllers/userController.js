4cff0521fb38e8d8696c9c1569ab6532282e52dc
Salim Bakri <sbakri@github.com>
2018-03-15 13:25:09 -0600

    all logic code implemented and passed testing

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -24,15 +24,36 @@ var UserController = function(User){
   }
 
   userCTRL.DeleteUser = function(req, res, next) {
-    req.status(204).send();
+    User.remove({_id : req.params.id }, function(err, user){
+      if(err) {
+        res.json({status: false, error: "faild to delete account"});
+      }
+      res.json({status: true, message: "account deleted"});
+    });
   }
 
   userCTRL.UpdateUserName = function(req, res, next) {
-    req.status(204).send();
+    User.findById(req.params.id, function(err, user){
+      user.username = req.body.username;
+      user.save(function(err, user){
+        if(err) {
+          res.json({status: false, error: "Username didn't updated"});
+        }
+        res.json({status: true, user: user});
+      });
+    });
   }
 
   userCTRL.ActivateAccount = function(req, res, next) {
-    req.status(204).send();
+    User.findById(req.params.id, function(err, user){
+      user.activated = true;
+      user.save(function(err, user){
+        if(err) {
+          res.json({status: false, error: "Username didn't updated"});
+        }
+        res.json({status: true, user: user});
+      });
+    });
   }
 
   return userCTRL;
